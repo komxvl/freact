15cd7115d35809519dc142ca940779e3e4047feb
komxvl <kostychenkomaxim@gmail.com>
2018-01-08 17:30:21 +0200

    Add files via upload

--- src/components/FormAddFilm/FormAddFilm.js
+++ src/components/FormAddFilm/FormAddFilm.js
@@ -5,83 +5,61 @@ class FormAddFilm extends Component {
     constructor(props){
         super(props);
         this.state = {
-            nameFilm: "",
-            descriptionFilm: "",
-            rating: "1",
-            error: ""
+            nameFilm: '',
+            descriptionFilm: '',
+            ratingFilm: '1',
+            chipsFilm: ''
         }
 
-        console.log(this.props);
+        this.handleChange = this.handleChange.bind(this);
+        this.submitedForm = this.submitedForm.bind(this);
     }
 
-getTitleFilm = (e) =>{
-    this.setState({
-        nameFilm: e.target.value
-    })
-    console.log(e.target.value);
-}
+    handleChange(e) {
+        this.setState({[e.target.name]: e.target.value})    
+    }
 
-getDescFilm = (e) =>{
-    this.setState({
-        descriptionFilm: e.target.value
-    })
-    console.log(e.target.value);
-}
-
-getRatingFilm = (e) =>{
-    this.setState({
-        rating: e.target.value
-    })
-    console.log(e.target.value);
-}
-
-getFilm = (e) =>{
-
-    e.preventDefault();
-    if(this.state.nameFilm === ""){
-        this.setState({
-            error: "Enter"
-        })
-     }
-     else{
-        let filmInfo = {
-            titleFilm:this.state.nameFilm,
-            descFilm: this.state.descriptionFilm,
-            ratingFilm: this.state.rating
+    submitedForm(e){
+        e.preventDefault();
+        if(this.state.nameFilm ==='' && this.state.descriptionFilm ===''){
+            alert("Enter form fild!");
         }
-
-        
-        console.log("FilmInfo",filmInfo);
-
-     }
-}
-
-
-
+        else{
+            this.props.addFilmInfo(this.state);
+            this.setState({
+                nameFilm: '',
+                descriptionFilm: '',
+                ratingFilm: '1',
+                chipsFilm: []
+            });
+            console.log("this.state",this.state);
+        }        
+    }
 
     pointRatingFilm = [1,2,3,4,5,6,7,8,9,10];
+
   render() {
     return (
-        <form className="MovieForm" onSubmit={this.props.getChildData}>
+        <form className="MovieForm" onSubmit={this.submitedForm}>
             <label className="MovieForm__label">Title
-                <input type="text" onChange={this.getTitleFilm} name="titleFilm" className="MovieForm__input"/>
-            </label>
-            <p style={{'color':'red'}}>
-                {
-                    (this.state.nameFilm === "") ? this.state.error : ""
-                }
-            </p>
+                <input type="text" value={this.state.nameFilm} onChange={this.handleChange} name="nameFilm" className="MovieForm__input"/>
+            </label>    
             <label className="MovieForm__label">Description
-                <textarea className="MovieForm__txtarea" name="descFilm" onChange={this.getDescFilm} rows="8" name="descFilm" value={this.state.descriptionFilm}> </textarea>
+                <textarea className="MovieForm__txtarea" name="descriptionFilm" value={this.state.descriptionFilm} onChange={this.handleChange} rows="8"> </textarea>
             </label>
             <label className="MovieForm__label">Rating
-                <select className="MovieForm__input" name="ratingFilm" onChange={this.getRatingFilm}>
+                <select className="MovieForm__input" name="ratingFilm"  onChange={this.handleChange}>
                     {this.pointRatingFilm.map((elem,index) =>{
                     return <option key={index} value={elem}>{elem}</option>
                     })
                     }
                 </select>
             </label>
+            <div className="MovieForm__genres">
+                <label><input type="checkbox" value="drama"  onChange={this.handleChange} />drama</label>
+                <label><input type="checkbox" value="action"  onChange={this.handleChange}/>action</label>
+                <label><input type="checkbox" value="comedy"  onChange={this.handleChange}/>comedy</label>
+            </div>
             <button className="MovieForm__submit" type="submit">submit</button>
         </form>
     )
