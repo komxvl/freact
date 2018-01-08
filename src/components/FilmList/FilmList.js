dad8a1c11565eea35292631d215b2d6c45cb1dd9
komxvl <kostychenkomaxim@gmail.com>
2018-01-08 17:29:37 +0200

    Add files via upload

--- src/components/FilmList/FilmList.js
+++ src/components/FilmList/FilmList.js
@@ -5,19 +5,28 @@ import FilmCard from '../FilmCard/FilmCard';
 class FilmList extends Component {
     constructor(props){
         super(props)
-        console.log("List props",this.props);
+        console.log("List props add",this.props);
+
+       
     }
+
     render() {             
-    return (
+    return ( 
         <div className="test">  
             {
                 this.props.filmArray.map((elem,index) =>(
-                    <FilmCard key = {elem.nameFilm} name = {elem.nameFilm} description = {elem.descriptionFilm} chipsFilm = {elem.chipsFilm} />
+                    <FilmCard   key = {elem.nameFilm}                         
+                                name = {elem.nameFilm} 
+                                description = {elem.descriptionFilm}
+                                rating = {elem.ratingFilm} 
+                                chipsFilm = {elem.chipsFilm}  
+                                removeFilmItem = {this.props.removeItem.bind(this,index)}                                      
+                    />
                 ))
             }
         </div>    
     );
-  }
+  } 
 }
 
 export default FilmList;
